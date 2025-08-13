7673e4177a65642974d2aaefb10bddc09cef50d3
Leo Tang <leotang634@gmail.com>
2025-08-13 11:01:59 -0400

    Forgot to build

--- docs/js/anims.js
+++ docs/js/anims.js
@@ -1,3 +1,21 @@
+function debounce(func, wait) {
+    let timeout;
+    return function() {
+        clearTimeout(timeout);
+        timeout = setTimeout(func, wait);
+    };
+}
+
 window.addEventListener("scroll", function() {
-    this.document.getElementsByClassName(".hr-anim")
+    const hrAnims = document.getElementsByClassName("hr anim")
+    Array.from(hrAnims).forEach( element => {
+        element.style.setProperty("--animation-duration", "20s")
+    })
+
+    debounce(() => {
+        Array.from(hrAnims).forEach(element => {
+            element.style.setProperty("--animation-duration", "120s");
+
+        });
+    }, 150)()
 })
