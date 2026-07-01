6bcaabc058ddc7425e74dc7954e523e6e314befb
Leo Tang <leotang634@gmail.com>
2026-06-30 21:54:38 -0400

    Forgot to build

--- docs/js/projects.js
+++ docs/js/projects.js
@@ -23,6 +23,51 @@ const projects = {
 		devpost: "",
 		ysc: "https://partner.projectboard.world/ysc/project/nexus-a-multimodal-device-for-deafblind-communication-using-hybrid-dynamic-speaker-diarization-ljyrrz/",
 	},
+
+    radius: {
+		title: "Radius",
+            text: "Radius is a native mobile app designed to connect local businesses with customers. It allows businesses to post polls, discounts, and announcements, increasing their reach within their communities. Radius was built with Expo React Native, the Places API, Drizzle ORM, and a Supabase backend. Radius won ninth place at FBLC's Canadian National Leadership Conference in the Coding and Programming event.",
+		image: "pictures/Radius2.png",
+		github: "https://github.com/acise03/fblcCP",
+		devpost: "",
+		ysc: "",
+	},
+
+    deltadispatch: {
+		title: "DeltaDispatch",
+            text: "DeltaDispatch is an AI-powered 911 dispatch system that prioritizes calls based on urgency, allowing dispatchers to address the most time-critical calls first. Compared to traditional dispatch systems, which prompt users to call again when all dispatchers are busy, DeltaDispatch uses the Gemini API to provide dispatchers with information even when they are busy. It was built with Twilio API, Gemini API, FastAPI, Redis, and Next.js at DeltaHacks",
+		image: "pictures/DeltaDispatch.png",
+		github: "https://github.com/Penguin60/deltahacks-xii",
+		devpost: "https://devpost.com/software/delta-dispatch",
+		ysc: "",
+	},
+
+    trashtag: {
+		title: "TrashTag",
+            text: "TrashTag is a native Android app that gamifies environmental cleanup. It awards points to users who report or pick up trash with photographic evidence. TrashTag was made with the Places API, a Firebase backend, and Android Kotlin. It won third place at IncubatorHacks.",
+		image: "pictures/TrashTag2.png",
+		github: "https://github.com/leotyes/TrashTagMobileApp",
+		devpost: "https://devpost.com/software/trashtag-o1itd0",
+		ysc: "",
+	},
+
+    leaf: {
+		title: "Leaf",
+            text: "Leaf is a native Android todo list with time- and location-based reminders. It was made with Android Kotlin, XML UI, Room database, and follows the MVVM architecture.",
+		image: "pictures/Leaf2.png",
+		github: "https://github.com/leotyes/TodoList",
+		devpost: "",
+		ysc: "",
+	},
+
+    studylocal: {
+		title: "StudyLocal",
+            text: "StudyLocal is a web app that allow students to find and compare the best study spots with several filters such as WiFi availability, accessibility, and noise level. It was made with vanilla HTML, CSS, JavaScript, and the Maps API at Ignition Hacks, my first hackathon.",
+		image: "pictures/StudyLocal2.jpg",
+		github: "https://github.com/STRSigmas/ignitionHacksSTRSigmas",
+		devpost: "https://devpost.com/software/studylocal",
+		ysc: "",
+	},
 };
 
 function setProject(id) {
